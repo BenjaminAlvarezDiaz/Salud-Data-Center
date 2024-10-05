b1fda52cabc84960e20108784d8b1495a58b3c96
BenjaminAlvarezDiaz <benja77744@gmail.com>
2024-10-05 01:02:46 -0300

    Mejorando sidebar y agregando la page help
    Mejorando el sidebar para que sea como en el maquetado y agregando la pantalla help que por el momento no tiene nada

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Perfil_Doctor from "./pages/Perfil_Doctor";
 import Iniciar_Sesion from "./pages/Inicio_Se";
 import Perfil_Empresa from "./pages/Perfil_Empresa"; // Agrega la importación
+import Help from "./pages/Help";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Iniciar_Sesion />} />
+        <Route path="/" element={<Help />} />
         <Route path="/Perfil" element={<Perfil_Doctor />} />
         <Route path="/PerfilEmp" element={<Perfil_Empresa />} />
+        <Route path="/Help" element={<Help />}/>
       </Routes>
     </div>
   );
